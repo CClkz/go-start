d48c909afbfa46d84f6bae7a47ddb9281bf65a7f
smj <sunmingjie@winupon.com>
2024-09-23 18:29:32 +0800

    feat: mysql

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module my-app
 go 1.22.2
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
---
+++ web/insert.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func insertData() {
+	r, err := Db.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	fmt.Println("insert succ:", id)
+}
