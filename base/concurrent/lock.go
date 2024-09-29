ea6638471b7f3bffcd66c3fdff13e014a467adc4
smj <sunmingjie@winupon.com>
2024-09-29 11:33:05 +0800

    feat: 原子操作

--- base/concurrent/lock.go
+++ base/concurrent/lock.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -30,7 +31,8 @@ func addByMutex() {
 	wg.Done()
 }
 
-func write(count int64) {
+// 读写锁
+func write(count int) {
 	rwlock.Lock() // 加写锁
 	x = x + 1
 	time.Sleep(10 * time.Millisecond) // 假设读操作耗时10毫秒
@@ -39,7 +41,7 @@ func write(count int64) {
 	wg.Done()
 }
 
-func read(count int64) {
+func read(count int) {
 	rwlock.RLock()               // 加读锁
 	time.Sleep(time.Millisecond) // 假设读操作耗时1毫秒
 	rwlock.RUnlock()             // 解读锁
@@ -47,25 +49,47 @@ func read(count int64) {
 	wg.Done()
 }
 
+// 原子操作版加函数
+func atomicAdd() {
+	// 原子操作是更底层的、硬件级别的同步机制，效率高于互斥锁
+	atomic.AddInt64(&x, 1)
+	wg.Done()
+}
+
 func tryLock() {
 	// wg.Add(2)
+
+	// 基本操作
 	// go add()
 	// go add()
+
+	// 加互斥锁
 	// go addByMutex()
 	// go addByMutex()
 
-	// 当读的操作很多时，用读写锁，让读操作可并发运行
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go write(i)
-	}
+	// 加读写锁
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go write(i)
+	// }
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go read(i)
+	// }
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10000; i++ {
 		wg.Add(1)
-		go read(i)
+		go atomicAdd()
 	}
 
 	wg.Wait()
 	// 期望是10000，但输出结果不一定是10000
 	fmt.Println(x)
 }
+
+// int 和 int64区别
+// int 32微系统是32位，64系统是64位
+// int64 确定为64位
+// for i := 0; i < 10; i++ 此处i为int类型
+// for i := int64(0); i < 10; i++ 此处为int64类型
+// int64(i) 将一个数值转为int64,同理int(i)
