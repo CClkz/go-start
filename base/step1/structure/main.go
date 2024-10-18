4a4ff8ec1bc4950e00615b78e62b78f970ef98b0
smj <sunmingjie@winupon.com>
2024-10-18 15:13:07 +0800

    perf: 调整目录结构

--- base/step1/structure/main.go
+++ base/step1/structure/main.go
@@ -8,7 +8,7 @@ import (
 	// 引入路径是目录
 	// 目录和包名一致时，简洁，推荐一致
 	// 目录和包名不一致，还是引入目录，程序里使用的是包名，这里编辑器自动加上了别名（===包名）
-	usermodels "my-app/base/user/model"
+	usermodels "my-app/base/step1/user/model"
 	// vars包取名main了，不能被引入
 	// vars "my-app/base/vars"
 )
